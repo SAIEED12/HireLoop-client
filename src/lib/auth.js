0b2d75c491e7a093c0081e7dbe86fd4581fcbbf4
SAIEED12 <abusaieed0@gmail.com>
2026-06-13 15:21:01 +0600

    feat: implement admin user management with role updates and user list retrieval

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,6 +4,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { admin } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db(process.env.AUTH_DB_NAME);
@@ -27,5 +28,8 @@ export const auth = betterAuth({
         input: false
       }
     }
-  }
+  },
+  plugins: [
+    admin()
+  ]
 });
